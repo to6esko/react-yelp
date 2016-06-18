aa889fc63747a6e149b6688b969b10a13010d6ac
Todor Krastev <to6esko69@gmail.com>
2016-06-18 17:11:26 +0300

    add css config and styles

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styles from './styles.module.css';
+import './app.css';
 const App = React.createClass({
   render: function() {
-    return (<div>Text text text</div>)
+      return (
+          <div className={styles['container']}>
+              Text text text
+          </div>
+      );
   }
 });
 
