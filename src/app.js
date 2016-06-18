ee7b0bc670cfeec96739363ca73ecdb53a5209aa
Todor Krastev <to6esko69@gmail.com>
2016-06-18 17:58:06 +0300

    add star

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import styles from './styles.module.css';
+import 'font-awesome/css/font-awesome.css';
 //import './app.css';
+
+
+
 const App = React.createClass({
   render: function() {
       return (
           <div className={styles.wrapper}>
-              <h1>Environmet: {__NODE_ENV__}</h1>
+              <h1>
+                  <i className="fa fa-star"></i>
+                  Environmet: {__NODE_ENV__}</h1>
           </div>
       );
   }
