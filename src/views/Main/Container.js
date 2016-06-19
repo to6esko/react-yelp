eeac4ae946627504b4387a191c5795794ed44f88
Todor Krastev <to6esko69@gmail.com>
2016-06-19 17:49:57 +0300

    add google places

--- src/views/Main/Container.js
+++ src/views/Main/Container.js
@@ -1,12 +1,45 @@
 import React from 'react';
 import Map,{GoogleApiWrapper} from 'google-maps-react';
+import {searchNearby} from 'utils/googleApiHelpers';
+
 
 class Container extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            places:[],
+            pagination:null
+        }
+    }
+    onReady(mapProps,map){
+        const {google} =this.props;
+        const opts={
+            location:map.center,
+            radius:'500',
+            types:['cafe']
+        }
+        searchNearby(google,map,opts)
+        .then((results,pagination)=>{
+            this.setState({
+                places:results,
+                pagination
+            })
+        }).catch((status,result)=>{
+
+        })
+    }
     render() {
         return (
             <div>
             Hello from container.
-            <Map google={this.props.google}/>
+            <Map 
+            onReady={this.onReady.bind(this)}
+            google={this.props.google}
+            visible={false}>
+            {this.state.places.map((place)=>{
+                return (<div key={place.id}>{place.name}</div>)
+            })}
+            </Map>
             </div>
         );
     }
